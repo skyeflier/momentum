224809443f398fab57095c454e1302c8dd4f523a
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-05-22 21:03:15 -0500

    removed controllers and routes

--- server/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')
-const router = app.Router()
-const { doRegister, doLogin, getUser } = require('../controllers/userController')
-
-app.post('/register', doRegister)
-app.post('/login', doLogin)
-app.get('/user', getUser)
-module.exports = router;
