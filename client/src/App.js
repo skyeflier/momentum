054a1c54e438b1d10479150aaec1f7cdec34ba50
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-05-24 19:55:00 -0500

    Added stripe functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
+import React, { useState, useEffect } from "react";
 import { ApolloProvider } from '@apollo/client';
 import './App.css';
 
 import Home from './components/pages/Home.js';
 import ApolloClient from 'apollo-boost';
-// import { Routes, Route } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -16,13 +16,58 @@ const client = new ApolloClient({
       }
     });
   },
-  uri: '/graphql',
 });
 
-function App() {
-  return <ApolloProvider client={client}>
-    < Home />
-  </ApolloProvider >
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return (
+    <ApolloProvider client={client}>
+      <Home /> {/* Include your other components here */}
+      {message ? (
+        <Message message={message} />
+      ) : (
+        <ProductDisplay />
+      )}
+    </ApolloProvider>
+  );
 }
 
-export default App;
+const ProductDisplay = () => (
+  <section>
+    <div className="product">
+      <img
+        src="client/public/mom-working-out.png"
+        alt="Icon of athletic mom with baby"
+      />
+      <div className="description">
+        <h3>MOMentum</h3>
+        <h5>$5.00</h5>
+      </div>
+    </div>
+    <form action="/create-checkout-session" method="POST">
+      <button type="submit">Checkout</button>
+    </form>
+  </section>
+);
--- server/server.js
+++ server/server.js
@@ -1,60 +1,53 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-// const { userRouter } = require('./routes/userRouter');
-
-// Require model 
-// const user = require('./models/User');
-
+const stripe = require('stripe')('sk_test_51NBQ9UILnn69mICeE8AVcRM5NfS3f3vhViIB3U6SSohQ1myDYw1pP2SLjfIIPgpsHaaPYSdJFDvMV0E92mta6IqO00ONUG53IW');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const MOMENTUM_DOMAIN = 'https://momentum-v1.herokuapp.com';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets
-// app.use('/images', express.static(path.join(__dirname, '../client/images')));
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: 'price_1NBR76ILnn69mICeTPQjDpqG',
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${MOMENTUM_DOMAIN}?success=true`,
+        cancel_url: `${MOMENTUM_DOMAIN}?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+});
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use('/api/users', userRouter);
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Set up home page
-// app.get('/', (req, res) => {
-//     res.send('Hello, Moms!');
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
+server.start().then(() => {
     server.applyMiddleware({ app });
 
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}!`);
             console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-        })
-    })
-};
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
-
-// 18:43
+        });
+    });
+});
